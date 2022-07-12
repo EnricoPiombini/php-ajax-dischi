2ce1ec84fcfac2da54ed3a22592090f47cc06e96
EnricoPiombini <piombini.enrico@gmail.com>
2022-07-12 16:45:05 +0200

    milestone1 senza css

--- milestone1/index.php
+++ milestone1/index.php
@@ -9,6 +9,7 @@ require_once "../api/db.php"
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
     <title>Php Albums</title>
 </head>
 
@@ -17,7 +18,7 @@ require_once "../api/db.php"
 
     <div class="container ">
 
-        <div class="row">
+        <div class="row row-cols-5">
 
             <?php foreach ($dbDiscs as $disc) { ?>
                 <div class="col">
